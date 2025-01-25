e125e1ebd9df2b1eb5e997bdbf625e00a9f4ae59
Yohana <thaysyohanacosta71@gmail.com>
2025-01-25 01:26:16 -0300

    fix: path bug

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 from components.sidebar import sidebar_navigation
-from paths import about
-from paths import analysis
-from paths import home
 
 import streamlit as st
 
@@ -14,10 +11,13 @@ selected_page = sidebar_navigation()
 
 
 if selected_page == "Home":
+    from paths import home
     home.show()
 if selected_page == "Analysis":
+    from paths import analysis
     analysis.show()
 if selected_page == "About":
+    from paths import about
     about.show()
 
 
