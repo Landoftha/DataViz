b9a0e1322ce1e40857582e859c4e3d4a1bfe3ded
Yohana <thaysyohanacosta71@gmail.com>
2025-01-25 00:58:47 -0300

    feat: best anime data

--- paths/analysis.py
+++ paths/analysis.py
@@ -6,7 +6,7 @@ from data.data_loader import load_data
 def show():
     st.title("Dashboard de Visualização de Dados")
 
-    file_path = "D:\Portifolio DATA SCIENCE\DataViz\data\Best_Animes_Data\data_animes_1000+_votes.csv"
+    file_path = "D:\Portifolio DATA SCIENCE\DataViz\data\BestAnimes\data_animes_1000+_votes.csv"
     try:
         df = load_data(file_path)
         st.write("Dados carregados com sucesso!")
