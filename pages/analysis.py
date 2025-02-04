2683231e30b537d88c47d8a53e52769dd689469b
Yohana <thaysyohanacosta71@gmail.com>
2025-02-03 23:53:18 -0300

    feat: chart of imdb rating

--- pages/analysis.py
+++ pages/analysis.py
@@ -10,9 +10,36 @@ sidebar_navigation()
 st.title("Análise estatística dos dados")
 
 file_path = "D:\Portifolio DATA SCIENCE\DataViz\data\BestAnimes\data_animes_1000+_votes.csv"
-try:
-    df = load_data(file_path)
-    st.dataframe(df)
-except Exception as e:
-    st.error(f"Erro ao carregar os dados: {e}")
+ 
+df = pd.read_csv(file_path) 
 
+st.dataframe(df)
+
+df_nota = df[df['IMDb Rating'] >= 8]
+
+st.dataframe(df_nota[["Title", "IMDb Rating"]])
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+plt.figure(figsize=(10, 6))
+sns.histplot(df["IMDb Rating"], bins=20, kde=True, color="royalblue")
+
+plt.xlabel("Nota IMDb")
+plt.ylabel("Quantidade de Animes")
+plt.title("Distribuição de Notas IMDb dos Animes")
+plt.grid(axis="y", linestyle="--", alpha=0.7)
+plt.show()
+
+st.pyplot(plt)
+
+plt.figure(figsize=(8, 5))
+sns.boxplot(x=df["IMDb Rating"], color="royalblue")
+
+plt.xlabel("Nota IMDb")
+plt.title("Boxplot das Notas IMDb dos Animes")
+plt.grid(axis="x", linestyle="--", alpha=0.7)
+plt.show()
+
+st.pyplot(plt)
